9318ddd24c1979cdf672421cf69127cb1554f9f9
wishtree-hsuthar <het.suthar@wishtreetech.com>
2023-04-06 19:07:21 +0530

    Master data JSON & shell script added

---
+++ import_masters.sh
@@ -0,0 +1,8 @@
+# Eg. Format:
+# mongoimport --db <database-name> --collection <collection-name> --file <file-name> --jsonArray
+
+mongoimport --db cgf_hrdd --collection roles --file roles.json --jsonArray
+mongoimport --db cgf_hrdd --collection countries --file countries.json --jsonArray
+mongoimport --db cgf_hrdd --collection masterlinks --file masterlinks.json --jsonArray
+
+
